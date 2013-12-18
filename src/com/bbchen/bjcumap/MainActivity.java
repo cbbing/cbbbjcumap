3b3e603de9ed1b0b367206743f4816cc8332328b
cbbing <cbbing@163.com>
2013-12-18 21:04:42 +0800

    setting 优化

--- src/com/bbchen/bjcumap/MainActivity.java
+++ src/com/bbchen/bjcumap/MainActivity.java
@@ -1,14 +1,23 @@
 package com.bbchen.bjcumap;
 
-import java.io.File;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.logging.Logger;
 
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.preference.PreferenceManager;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
 import android.graphics.Bitmap;
 import android.util.Log;
 import android.view.KeyEvent;
@@ -19,6 +28,7 @@ import android.webkit.WebSettings.RenderPriority;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.webkit.WebSettings.PluginState;
+import android.widget.Toast;
 
 @SuppressLint("SetJavaScriptEnabled")
 public class MainActivity extends Activity {
@@ -26,6 +36,8 @@ public class MainActivity extends Activity {
 	private static final String TAG = MainActivity.class.getSimpleName();  
     private static final String APP_CACHE_DIRNAME = "/webcache"; 
 	WebView webView;
+	private long firstime = 0;
+	String defaultURL = "http://218.246.23.89/bjcumap/index_mobile.jsp";
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -36,86 +48,76 @@ public class MainActivity extends Activity {
 		initWebView();
 		
 		 //加载需要显示的网页 
-		webView.loadUrl("http://218.246.23.85/bjcumap/index.jsp");
+		webView.loadUrl(defaultURL);
+		//webView.loadUrl("http://www.baidu.com");
 		//点击链接继续在当前browser中响应，而不是新开Android的系统browser中响应该链接，必须覆盖 webview的WebViewClient对象
-		webView.setWebViewClient(new HelloWebViewClient());
+		webView.setWebViewClient(new CbbWebViewClient());
 		
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		//getMenuInflater().inflate(R.menu.main, menu);
-		Intent intent = new Intent(this, SettingActivity.class);
-		startActivity(intent);
-		return true;
-	}
-
-	@Override
-	//设置回退 
-	public boolean onKeyDown(int keyCode, KeyEvent event) {
+	protected void onResume() {
 		// TODO Auto-generated method stub
-		if (keyCode == KeyEvent.KEYCODE_BACK && webView.canGoBack()) {
-			webView.goBack();
-			return true;
+		super.onResume();
+		SharedPreferences prefs =PreferenceManager.getDefaultSharedPreferences(this) ;
+     
+		//修改主页
+        String userUrl = prefs.getString("set_url","");//修改后的url
+        String userUrlPre = prefs.getString("set_url_pre",defaultURL);//修改之前的url
+        if (!userUrl.contains("http://")) {
+        	userUrl = "http://" + userUrl;
+		} 
+        if (!userUrlPre.contains("http://")) {
+        	userUrlPre = "http://" + userUrlPre;
+		} 
+        if (!userUrl.equals(userUrlPre)) {
+            if (checkURL(userUrl))
+            {
+            	webView.loadUrl(userUrl);
+            }
+            else {
+            	prefs.edit().putString("set_url", defaultURL);
+            	webView.loadUrl(defaultURL);
+    		}	
+            prefs.edit().putString("set_url_pre", userUrl);
+            prefs.edit().commit();
 		}
-		
-		return super.onKeyDown(keyCode, event);
+        
+        
+        //设置旋转
+     	Boolean bAutoRotate = prefs.getBoolean("set_rotate",false);//false表示没有查到checkbox这个key的返回值
+     	changeOrientation(bAutoRotate);
+     	
+     	//设置缩放
+     	Boolean bZoom = prefs.getBoolean("set_zoom", false);
+     	webView.getSettings().setBuiltInZoomControls(bZoom);	
+     	webView.getSettings().setSupportZoom(true);
+     	webView.getSettings().setUseWideViewPort(bZoom);
+     	
+     	//设置缓存模式
+     	Boolean bCacheMode = prefs.getBoolean("set_cacheMode",false);
+     	if (bCacheMode) {
+     		webView.getSettings().setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+     	}
+     	else {
+     		webView.getSettings().setCacheMode(WebSettings.LOAD_DEFAULT);
+     	}
+     	
 	}
-
-	 //Web视图 
-    private class HelloWebViewClient extends WebViewClient { 
-        @Override
-        public boolean shouldOverrideUrlLoading(WebView view, String url) { 
-        	Log.i(TAG, "intercept url="+url); 
-        	view.loadUrl(url); 
-            return true; 
-        }
-
-		/*@Override
-		public void onLoadResource(WebView view, String url) {
-			// TODO Auto-generated method stub
-			Log.i(TAG, "onLoadResource url="+url);
-			super.onLoadResource(view, url);
-		}
-
-		@Override
-		public void onPageStarted(WebView view, String url, Bitmap favicon) {
-			// TODO Auto-generated method stub
-			Log.e(TAG, "onPageStarted");  
-			super.onPageStarted(view, url, favicon);
-		}
-
-		@Override
-		public void onPageFinished(WebView view, String url) {
-			// TODO Auto-generated method stub
-			String title = view.getTitle();  
-            Log.e(TAG, "onPageFinished WebView title=" + title);
-			super.onPageFinished(view, url);
-		}
-
-		@Override
-		public void onReceivedError(WebView view, int errorCode,
-				String description, String failingUrl) {
-			// TODO Auto-generated method stub
-			super.onReceivedError(view, errorCode, description, failingUrl);
-		} */
-    } 
-	
 	private void initWebView() {
 		//设置WebView属性，能够执行Javascript脚本 
 		WebSettings settings = webView.getSettings();
 		settings.setPluginState(PluginState.ON);
 		settings.setJavaScriptEnabled(true);
-		settings.setRenderPriority(RenderPriority.HIGH);//提高渲染的优先级,提高加载速度
+		//settings.setRenderPriority(RenderPriority.HIGH);//提高渲染的优先级,提高加载速度
 		//settings.setBlockNetworkImage(true);//把图片加载放在最后来加载渲染
 		//设置 缓存模式
-		if (isNetworkConnected(this)) {
-			settings.setCacheMode(WebSettings.LOAD_DEFAULT);
-		}
-		else {
-			settings.setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
-		}
+		//if (isNetworkConnected(this)) {
+		//	settings.setCacheMode(WebSettings.LOAD_DEFAULT);
+		//}
+		//else {
+		//	settings.setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+		//}
 		
 		
 		// 开启 DOM storage API 功能  
@@ -137,11 +139,78 @@ public class MainActivity extends Activity {
 		settings.setAllowFileAccess(true);
 		settings.setDefaultTextEncodingName("UTF-8");
 		settings.setAllowFileAccessFromFileURLs(true);
-		if (android.os.Build.VERSION.SDK_INT >= 14) {
-			webView.setLayerType(View.LAYER_TYPE_HARDWARE, null);
-		}
-		settings.setSupportZoom(true);
+		//if (android.os.Build.VERSION.SDK_INT >= 14) {
+			//webView.setLayerType(View.LAYER_TYPE_HARDWARE, null);
+		//}
+		
 	}
+	@Override
+	//设置回退 
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		// TODO Auto-generated method stub
+		if (keyCode == KeyEvent.KEYCODE_BACK && webView.canGoBack()) {
+			webView.goBack();
+			long secondtime = System.currentTimeMillis();
+			if (secondtime - firstime > 2000) {
+				Toast.makeText(MainActivity.this, "再按一次返回键退出", Toast.LENGTH_SHORT).show();
+				firstime = System.currentTimeMillis();
+				return true;
+			} else {
+				finish();
+				System.exit(0);
+			}
+			
+		}
+		else if (keyCode == KeyEvent.KEYCODE_MENU)
+		{
+			Intent intent = new Intent(this, SettingActivity.class);
+			startActivity(intent);
+			//finish();
+		}
+		
+		return super.onKeyDown(keyCode, event);
+	}
+
+	 //Web视图 
+    private class CbbWebViewClient extends WebViewClient { 
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) { 
+        	Log.i(TAG, "intercept url="+url); 
+        	view.loadUrl(url); 
+            return true; 
+        }
+
+		@Override
+		public void onLoadResource(WebView view, String url) {
+			// TODO Auto-generated method stub
+			Log.i(TAG, "onLoadResource url="+url);
+			super.onLoadResource(view, url);
+		}
+
+		@Override
+		public void onPageStarted(WebView view, String url, Bitmap favicon) {
+			// TODO Auto-generated method stub
+			Log.i(TAG, "onPageStarted");  
+			super.onPageStarted(view, url, favicon);
+		}
+
+		@Override
+		public void onPageFinished(WebView view, String url) {
+			// TODO Auto-generated method stub
+			String title = view.getTitle();  
+            Log.i(TAG, "onPageFinished WebView title=" + title);
+			super.onPageFinished(view, url);
+		}
+
+		@Override
+		public void onReceivedError(WebView view, int errorCode,
+				String description, String failingUrl) {
+			// TODO Auto-generated method stub
+			super.onReceivedError(view, errorCode, description, failingUrl);
+		} 
+    } 
+	
+	
     
     public boolean isNetworkConnected(Context context) {  
         if (context != null) {  
@@ -154,4 +223,56 @@ public class MainActivity extends Activity {
         }  
         return false;  
     }
+    
+	@SuppressLint("NewApi")
+	public boolean checkURL(String url) {
+		if (!url.contains("http://")) {
+			url = "http://" + url;
+		}
+		StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder().detectDiskReads().detectDiskWrites().detectNetwork().penaltyLog().build()); 
+	
+		boolean value = false;
+		try {
+			HttpURLConnection conn = (HttpURLConnection) new URL(url)
+					.openConnection();
+			int code = conn.getResponseCode();
+			System.out.println(">>>>>>>>>>>>>>>> " + code
+					+ " <<<<<<<<<<<<<<<<<<");
+			if (code != 200) {
+				value = false;
+			} else {
+				value = true;
+			}
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return value;
+	}
+	
+	/*
+	 * 通过API动态改变当前屏幕的显示方向
+	 */
+	public void changeOrientation(Boolean bAutoRotate) {
+		
+		if (!bAutoRotate) {
+			setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED);
+		}
+		else {
+			setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+			
+			// 取得当前屏幕方向
+//			Configuration config = getResources().getConfiguration();   
+//		    if (config.orientation == Configuration.ORIENTATION_LANDSCAPE){   
+//		            //横屏，比如 480x320
+//		    	setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+//		     }else if(config.orientation == Configuration.ORIENTATION_PORTRAIT){   
+//		            //竖屏 ，标准模式 320x480
+//		    	 setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+//		     }
+		}
+	}
 }
--- src/com/bbchen/bjcumap/SettingActivity.java
+++ src/com/bbchen/bjcumap/SettingActivity.java
@@ -1,10 +1,17 @@
 package com.bbchen.bjcumap;
 
 import java.io.File;
+import java.lang.reflect.Method;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
+import android.content.pm.IPackageStatsObserver;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageStats;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.preference.CheckBoxPreference;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceChangeListener;
@@ -12,15 +19,19 @@ import android.preference.Preference.OnPreferenceClickListener;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceManager;
 import android.util.Log;
+import android.view.KeyEvent;
+import android.widget.Toast;
 
 public class SettingActivity extends PreferenceActivity 
 			implements OnPreferenceChangeListener, OnPreferenceClickListener{
 
 	private static final String TAG = MainActivity.class.getSimpleName();  
     private static final String APP_CACHE_DIRNAME = "/webcache"; 
-    private CheckBoxPreference mCheckRotate, mCheckZoom;
+    //private CheckBoxPreference mCheckRotate, mCheckZoom;
     private Preference mPreferenceClearCache;
     private SharedPreferences mSharedPrefs;
+    
+    private static final String ATTR_PACKAGE_STATS="PackageStats";
     
 	@SuppressWarnings("deprecation")
 	@Override
@@ -31,18 +42,19 @@ public class SettingActivity extends PreferenceActivity
 		addPreferencesFromResource(R.xml.setting);
 		
 		initViews();
+		getpkginfo("com.bbchen.bjcumap");
 	}
 
 	private void initViews(){
 		//得到以包命名的SharedPreferences
 		mSharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
-		mCheckRotate = (CheckBoxPreference)findPreference("set_rotate");
-		mCheckZoom = (CheckBoxPreference)findPreference("set_zoom");
-		mPreferenceClearCache = (Preference)findPreference("set_clearcache");
+		//mCheckRotate = (CheckBoxPreference)findPreference("set_rotate");
+		//mCheckZoom = (CheckBoxPreference)findPreference("set_zoom");
+		//mPreferenceClearCache = (Preference)findPreference("set_clearcache");
 		 
-		mCheckRotate.setOnPreferenceChangeListener(this);
-		mCheckZoom.setOnPreferenceChangeListener(this);
-		mPreferenceClearCache.setOnPreferenceClickListener(this);
+		//mCheckRotate.setOnPreferenceChangeListener(this);
+		//mCheckZoom.setOnPreferenceChangeListener(this);
+		//mPreferenceClearCache.setOnPreferenceClickListener(this);
 		
 	}
 
@@ -61,10 +73,10 @@ public class SettingActivity extends PreferenceActivity
          
        //WebView 缓存文件  
        File appCacheDir = new File(getFilesDir().getAbsolutePath()+APP_CACHE_DIRNAME);  
-       Log.e(TAG, "appCacheDir path="+appCacheDir.getAbsolutePath());  
+       Log.i(TAG, "appCacheDir path="+appCacheDir.getAbsolutePath());  
          
        File webviewCacheDir = new File(getCacheDir().getAbsolutePath()+"/webviewCache");  
-       Log.e(TAG, "webviewCacheDir path="+webviewCacheDir.getAbsolutePath());  
+       Log.i(TAG, "webviewCacheDir path="+webviewCacheDir.getAbsolutePath());  
          
        //删除webview 缓存目录  
        if(webviewCacheDir.exists()){  
@@ -101,25 +113,114 @@ public class SettingActivity extends PreferenceActivity
    }
 
 
-@Override
-public boolean onPreferenceClick(Preference preference) {
-	// TODO Auto-generated method stub
-	if (preference.getKey().equals("set_clearcache")) {
-		Log.d("TAG", "clearcache");
-		clearWebViewCache();
+	@Override
+	public boolean onPreferenceClick(Preference preference) {
+		// TODO Auto-generated method stub
+		if (preference.getKey().equals("set_clearcache")) {
+			Log.d("TAG", "clearcache");
+			clearWebViewCache();
+		}
+		else if (preference.getKey().equals("set_url")) {
+			Log.d("TAG", "clearcache");
+			clearWebViewCache();
+		}
+		return false;
 	}
-	return false;
-}
 
+	@Override
+	public boolean onPreferenceChange(Preference preference, Object newValue) {
+		// TODO Auto-generated method stub
+		if (preference.getKey().equals("set_rotate")) {
+			Log.d("TAG", "rotate");
+		} else if (preference.getKey().equals("set_zoom")) {
+			Log.d("TAG", "zoom");
+		}
+		return false;
+	} 
 
-@Override
-public boolean onPreferenceChange(Preference preference, Object newValue) {
-	// TODO Auto-generated method stub
-	if (preference.getKey().equals("set_rotate")) {
-		Log.d("TAG", "rotate");
-	}else if (preference.getKey().equals("set_zoom")) {
-		Log.d("TAG", "zoom");
+	@Override
+	//设置回退 
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		// TODO Auto-generated method stub
+		if (keyCode == KeyEvent.KEYCODE_BACK ) {
+			//Intent intent = new Intent(this, MainActivity.class);
+			//startActivity(intent);
+			finish();
+			return true;
+		}
+		
+		return super.onKeyDown(keyCode, event);
 	}
-	return false;
-}  
+	
+	private Handler mHandler = new Handler() {
+		public void handleMessage(Message msg) {
+			switch (msg.what) {
+			case 1:
+				String infoString = "";
+				PackageStats newPs = msg.getData().getParcelable(
+						ATTR_PACKAGE_STATS);
+				if (newPs != null) {
+					infoString += "应用程序大小: " + formatFileSize(newPs.codeSize);
+					infoString += "\n数据大小: " + formatFileSize(newPs.dataSize);
+					infoString += "\n缓存大小: " + formatFileSize(newPs.cacheSize);
+				}
+				//tv.setText(infoString);
+				Log.e("cache", infoString);
+				//Toast.makeText(this, infoString, Toast.LENGTH_SHORT).show();
+				break;
+			default:
+				break;
+			}
+		}
+	};
+
+	public void getpkginfo(String pkg) {
+		PackageManager pm = getPackageManager();
+		try {
+			Method getPackageSizeInfo = pm.getClass().getMethod(
+					"getPackageSizeInfo", String.class,
+					IPackageStatsObserver.class);
+			getPackageSizeInfo.invoke(pm, pkg, new PkgSizeObserver());
+		} catch (Exception e) {
+		}
+	}
+
+	class PkgSizeObserver extends IPackageStatsObserver.Stub {
+		public void onGetStatsCompleted(PackageStats pStats, boolean succeeded) {
+			Message msg = mHandler.obtainMessage(1);
+			Bundle data = new Bundle();
+			data.putParcelable(ATTR_PACKAGE_STATS, pStats);
+			msg.setData(data);
+			mHandler.sendMessage(msg);
+
+		}
+	}
+ 
+
+	/**
+	 * 获取文件大小
+	 * 
+	 * @param length
+	 * @return
+	 */
+	public static String formatFileSize(long length) {
+		String result = null;
+		int sub_string = 0;
+		if (length >= 1073741824) {
+			sub_string = String.valueOf((float) length / 1073741824).indexOf(
+					".");
+			result = ((float) length / 1073741824 + "000").substring(0,
+					sub_string + 3) + "GB";
+		} else if (length >= 1048576) {
+			sub_string = String.valueOf((float) length / 1048576).indexOf(".");
+			result = ((float) length / 1048576 + "000").substring(0,
+					sub_string + 3) + "MB";
+		} else if (length >= 1024) {
+			sub_string = String.valueOf((float) length / 1024).indexOf(".");
+			result = ((float) length / 1024 + "000").substring(0,
+					sub_string + 3) + "KB";
+		} else if (length < 1024)
+			result = Long.toString(length) + "B";
+		return result;
+	}
 }
