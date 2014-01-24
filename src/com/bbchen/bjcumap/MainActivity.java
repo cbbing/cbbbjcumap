bbbb1f731d628899e8fa2cc792d8e04f490214fa
cbbing <cbbing@163.com>
2014-01-24 17:37:26 +0800

    捕捉下载链接，后台下载

--- src/com/bbchen/bjcumap/MainActivity.java
+++ src/com/bbchen/bjcumap/MainActivity.java
@@ -1,31 +1,50 @@
 package com.bbchen.bjcumap;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLDecoder;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
 
 import com.bbchen.util.ActivityList;
 
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.StrictMode;
 import android.preference.PreferenceManager;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.DialogInterface.OnDismissListener;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.ActivityInfo;
 import android.graphics.Bitmap;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.KeyEvent;
 import android.view.View;
 import android.webkit.CookieManager;
 import android.webkit.CookieSyncManager;
+import android.webkit.DownloadListener;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -39,7 +58,8 @@ public class MainActivity extends Activity {
     private static final String APP_CACHE_DIRNAME = "/webcache"; 
 	WebView webView;
 	private long firstime = 0;
-	public static String defaultURL = "http://www.bjwhmap.com/bjcumap/index_mobile.jsp?mobileFlag=MOBILE";//"http://wap.baidu.com";//
+	//public static String defaultURL = "http://www.bjwhmap.com/bjcumap/functionZone/main/video.jsp?videoName=%E6%96%87%E5%8C%96%E7%A7%91%E6%8A%80%E8%9E%8D%E5%90%88%E7%A4%BA%E8%8C%83%E5%8A%9F%E8%83%BD%E5%8C%BA&videoUrl=1";
+	public static String defaultURL = "http://www.bjwhmap.com/bjcumap/index_mobile.jsp?mobileFlag=MOBILE";////
 	
 	@SuppressLint("NewApi")
 	@Override
@@ -104,6 +124,7 @@ public class MainActivity extends Activity {
 		//点击链接继续在当前browser中响应，而不是新开Android的系统browser中响应该链接，必须覆盖 webview的WebViewClient对象
 		webView.setWebViewClient(new CbbWebViewClient());
 		
+		webView.setDownloadListener(new MyWebViewDownLoadListener());
 		ActivityList.activityListAll.add(this);
 	}
 
@@ -290,7 +311,245 @@ public class MainActivity extends Activity {
 		} 
     } 
 	
-	
+    //DownListener 文件下载
+    private class CbbWebViewDownLoadListener implements DownloadListener{
+
+        @Override
+        public void onDownloadStart(String url, String userAgent, String contentDisposition, String mimetype,
+                                    long contentLength) {        	
+        	Log.i("tag", "url="+url);        	
+        	Log.i("tag", "userAgent="+userAgent);
+        	Log.i("tag", "contentDisposition="+contentDisposition);        	
+        	Log.i("tag", "mimetype="+mimetype);
+        	Log.i("tag", "contentLength="+contentLength);
+            Uri uri = Uri.parse(url);
+            Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+            startActivity(intent);        	 
+        }
+    }
+
+	// 内部类
+	private class MyWebViewDownLoadListener implements DownloadListener {
+
+		@Override
+		public void onDownloadStart(String url, String userAgent,
+				String contentDisposition, String mimetype, long contentLength) {
+			if (!Environment.getExternalStorageState().equals(
+					Environment.MEDIA_MOUNTED)) {
+				Toast t = Toast.makeText(MainActivity.this, "需要SD卡。",
+						Toast.LENGTH_LONG);
+				t.setGravity(Gravity.CENTER, 0, 0);
+				t.show();
+				return;
+			}
+			DownloaderTask task = new DownloaderTask();
+			task.execute(url);
+		}
+
+	}
+
+	// 内部类
+	private class DownloaderTask extends AsyncTask<String, Void, String> {
+
+		public DownloaderTask() {
+		}
+
+		@Override
+		protected String doInBackground(String... params) {
+			// TODO Auto-generated method stub
+			String url = params[0];
+			// Log.i("tag", "url="+url);
+			String fileName = url.substring(url.lastIndexOf("/") + 1);
+			fileName = URLDecoder.decode(fileName);
+			Log.i("tag", "fileName=" + fileName);
+
+			File directory = Environment.getExternalStorageDirectory();
+			File file = new File(directory, fileName);
+			if (file.exists()) {
+				Log.i("tag", "The file has already exists.");
+				return fileName;
+			}
+			try {
+				HttpClient client = new DefaultHttpClient();
+				// client.getParams().setIntParameter("http.socket.timeout",3000);//设置超时
+				HttpGet get = new HttpGet(url);
+				HttpResponse response = client.execute(get);
+				if (HttpStatus.SC_OK == response.getStatusLine()
+						.getStatusCode()) {
+					HttpEntity entity = response.getEntity();
+					InputStream input = entity.getContent();
+
+					writeToSDCard(fileName, input);
+
+					input.close();
+					// entity.consumeContent();
+					return fileName;
+				} else {
+					return null;
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+				return null;
+			}
+		}
+
+		@Override
+		protected void onCancelled() {
+			// TODO Auto-generated method stub
+			super.onCancelled();
+		}
+
+		@Override
+		protected void onPostExecute(String result) {
+			// TODO Auto-generated method stub
+			super.onPostExecute(result);
+			closeProgressDialog();
+			if (result == null) {
+				Toast t = Toast.makeText(MainActivity.this, "连接错误！请稍后再试！",
+						Toast.LENGTH_LONG);
+				t.setGravity(Gravity.CENTER, 0, 0);
+				t.show();
+				return;
+			}
+
+			Toast t = Toast.makeText(MainActivity.this, "已保存到SD卡。",
+					Toast.LENGTH_LONG);
+			t.setGravity(Gravity.CENTER, 0, 0);
+			t.show();
+			File directory = Environment.getExternalStorageDirectory();
+			File file = new File(directory, result);
+			Log.i("tag", "Path=" + file.getAbsolutePath());
+
+			Intent intent = getFileIntent(file);
+
+			startActivity(intent);
+
+		}
+
+		@Override
+		protected void onPreExecute() {
+			// TODO Auto-generated method stub
+			super.onPreExecute();
+			//showProgressDialog();
+			Toast.makeText(MainActivity.this, "已开始后台加载，请等待...", Toast.LENGTH_LONG).show();
+		}
+
+		@Override
+		protected void onProgressUpdate(Void... values) {
+			// TODO Auto-generated method stub
+			super.onProgressUpdate(values);
+		}
+		
+		private ProgressDialog mDialog;
+		private void showProgressDialog(){
+			if(mDialog==null){
+				mDialog = new ProgressDialog(MainActivity.this);  
+				mDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);//设置风格为圆形进度条  
+				mDialog.setMessage("正在加载 ，请等待...");  
+				mDialog.setIndeterminate(false);//设置进度条是否为不明确  
+				mDialog.setCancelable(true);//设置进度条是否可以按退回键取消  
+				mDialog.setCanceledOnTouchOutside(false);
+				mDialog.setOnDismissListener(new OnDismissListener() {
+					
+					public void onDismiss(DialogInterface dialog) {
+						// TODO Auto-generated method stub
+						mDialog=null;
+					}
+				});
+				mDialog.show();
+				
+			}
+		}
+		private void closeProgressDialog(){
+			if(mDialog!=null){
+				mDialog.dismiss();
+				mDialog=null;
+			}
+		}
+		 public Intent getFileIntent(File file){
+//			 Uri uri = Uri.parse("http://m.ql18.com.cn/hpf10/1.pdf");
+			Uri uri = Uri.fromFile(file);
+			String type = getMIMEType(file);
+			Log.i("tag", "type="+type);
+		    Intent intent = new Intent("android.intent.action.VIEW");
+		    intent.addCategory("android.intent.category.DEFAULT");
+		    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+		    intent.setDataAndType(uri, type);
+		    return intent;
+		  }
+		 
+		public void writeToSDCard(String fileName,InputStream input){
+			
+			if(Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)){
+				File directory=Environment.getExternalStorageDirectory();
+				File file=new File(directory,fileName);
+//				if(file.exists()){
+//					Log.i("tag", "The file has already exists.");
+//					return;
+//				}
+				try {
+					FileOutputStream fos = new FileOutputStream(file);
+					byte[] b = new byte[2048];
+					int j = 0;
+					while ((j = input.read(b)) != -1) {
+						fos.write(b, 0, j);
+					}
+					fos.flush();
+					fos.close();
+				} catch (FileNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}else{
+				Log.i("tag", "NO SDCard.");
+			}
+		}
+		
+		private String getMIMEType(File f){   
+	      String type="";  
+	      String fName=f.getName();  
+	      /* 取得扩展名 */  
+	      String end=fName.substring(fName.lastIndexOf(".")+1,fName.length()).toLowerCase();
+	      
+	      /* 依扩展名的类型决定MimeType */
+	      if(end.equals("pdf")){
+	    	  type = "application/pdf";//
+	      }
+	      else if(end.equals("m4a")||end.equals("mp3")||end.equals("mid")||  
+	      end.equals("xmf")||end.equals("ogg")||end.equals("wav")){  
+	        type = "audio/*";   
+	      }  
+	      else if(end.equals("3gp")||end.equals("mp4")){  
+	        type = "video/*";  
+	      }  
+	      else if(end.equals("jpg")||end.equals("gif")||end.equals("png")||  
+	      end.equals("jpeg")||end.equals("bmp")){  
+	        type = "image/*";  
+	      }  
+	      else if(end.equals("apk")){   
+	        /* android.permission.INSTALL_PACKAGES */   
+	        type = "application/vnd.android.package-archive"; 
+	      }
+//	      else if(end.equals("pptx")||end.equals("ppt")){
+//	    	  type = "application/vnd.ms-powerpoint"; 
+//	      }else if(end.equals("docx")||end.equals("doc")){
+//	    	  type = "application/vnd.ms-word";
+//	      }else if(end.equals("xlsx")||end.equals("xls")){
+//	    	  type = "application/vnd.ms-excel";
+//	      }
+	      else{
+//	    	  /*如果无法直接打开，就跳出软件列表给用户选择 */  
+	        type="*/*";
+	      }
+	      return type;
+	    }   
+
+
+	} 
+
     
     public boolean isNetworkConnected(Context context) {  
         if (context != null) {  
